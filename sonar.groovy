0874df7acec7e44dca1c75f495e3de2529dd2004
saif <saif@gmail.com>
2022-04-12 16:27:40 +0530

    sonar update

--- sonar.groovy
+++ sonar.groovy
@@ -6,5 +6,15 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Ashmirasheikh/studentapp-ui.git'
             }
         }
+        stage('build') {
+            steps {
+                sh 'mvn package clean'
+            }
+        }
+         stage('test') {
+            steps { withsonarqube env ('sonar')
+                sh 'mvn sonar:sonar'
+            }
+        }
     }
 }
