bd1fe8a7fdbb2fdd9f5b9cc09d263d5742e21b2f
saif <saif@gmail.com>
2022-04-12 22:00:15 +0530

    new sonarqube file

---
+++ sonarqube.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('pull') {
+            steps {
+                git 'https://github.com/Ashmirasheikh/webhook.git'
+            }
+        }
+         stage('build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('test') {
+            steps { withSonarQubeEnv(credentialsId: '21') {
+                mvn 'sonar:sonar'
+                }
+            }
+        }
+    }
+}
